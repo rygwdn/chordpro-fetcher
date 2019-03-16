288c0a04a4e192dd5fe625d340445866ca48291f
Ryan Wooden <rygwdn@gmail.com>
2019-03-15 23:14:46 -0300

    add shortcut.run method.

--- src/shortcut.js
+++ src/shortcut.js
@@ -1,11 +1,28 @@
 import * as wt from './worshiptogether'
 import * as ug from './ultimateguitar'
 
-
-export async function getFromWT() {
+async function getFromWT() {
   return await wt.buildFile($, async (url) => await (await fetch(url)).text())
 }
 
-export async function getFromUG() {
+async function getFromUG() {
   return await ug.buildFile(UGAPP.store.page.data)
+}
+
+export async function getFile() {
+  if (window.location.hostname.match(/\.ultimate-guitar\.com$/)) {
+    return await getFromUG()
+  }
+
+  if (window.location.hostname.match(/\.worshiptogether\.com$/)) {
+    return await getFromWT()
+  }
+
+  throw new Error('Unrecognized page')
+}
+
+export async function run() {
+  getFile()
+    .then(file => completion(file))
+    .catch(err => completion('ERROR: ' + err))
 }
